72927c8f9098afacd6c8ba35dbae50932d73beae
Kevin Patel <kpatel@iitg.ac.in>
2022-08-15 17:46:58 +0530

    setting up agora

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -1,1 +1,29 @@
-const APP_ID = ""
+const APP_ID = "";
+
+let uid = sessionStorage.getItem('uid');
+
+if(!uid){
+    uid = String(Math.floor(Math.random()*10000));
+    sessionStorage.setItem('uid',uid);
+}
+
+let token = null;
+let client;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get('room');
+
+if (!roomId){
+    roomId = "main";
+}
+
+let localTrack = [];
+let remoteUsers = {};
+
+let joinRoomInit = async() => {
+    client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'});
+    await client.join(APP_ID,roomId,token,uid);
+}
+
+joinRoomInit();
